85b75f0964e611a75c33298ea9c87a2d3331baa7
Luan Rodrigues <rodriguesluandev@gmail.com>
2023-09-12 18:05:33 -0300

    fix: new root

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ const express = require("express");
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.get("/:nome/:lang", (req, res) => {
-    let nome = req.params.nome;
-    let lang = req.params.lang;
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        data: "25/08/2023"
-    });
-})
+app.get("/",(req, res) => {
+    res.render("index");
+});
+
+app.get("/question",(req, res) => {
+    res.render("question");
+});
 
 // Initiating server
 app.listen(8080, function(error){

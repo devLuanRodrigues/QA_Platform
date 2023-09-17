9fdf75cc3f56e543b11b249a847854906ba78049
Luan Rodrigues <rodriguesluandev@gmail.com>
2023-09-17 12:56:55 -0300

    fix: db config

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('qaplatform', 'root', 'C@nurun1t', {
+const connection = new Sequelize('qaplatform', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
 });
